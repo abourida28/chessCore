a8f697c62d8b3222e62319b196cc09722b02db49
Abourida <omarabourida2015@gmail.com>
2023-11-15 09:49:09 +0200

    Initialized project

---
+++ chessCore/src/main/java/com/mycompany/chesscore/ChessCore.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.chesscore;
+
+/**
+ *
+ * @author omara
+ */
+public class ChessCore {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
