8df1cab56b63479fbda3c4102502806898a399a6
Abourida <omarabourida2015@gmail.com>
2023-11-16 02:17:42 +0200

    Made rook class

---
+++ chessCore/src/main/java/com/mycompany/chesscore/pieces/Rook.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.chesscore.pieces;
+
+import com.mycompany.chesscore.ChessBoard;
+import com.mycompany.chesscore.Square;
+import com.mycompany.chesscore.constants;
+import com.mycompany.chesscore.constants.Letter;
+
+/**
+ *
+ * @author omara
+ */
+public class Rook extends Piece {
+
+    public Rook(constants.Color color, int row, constants.Letter column, ChessBoard board) {
+        super(color, row, column, board);
+    }
+
+    @Override
+    public boolean isValidMove(Square target) {
+        int targetV = target.getRow();
+        Letter targetH = target.getColumn();
+        Square[][] board = super.getBoard().board;
+        //First check that target is empty or enemy
+        if (target.getPiece() != null) {
+            if (target.getPiece().getColor() == super.getColor()) {
+                return false;
+            }
+        }
+        //check that rook is moving on a row or a column
+        if (targetH == super.column) {
+            Square s;
+            //check rook moving up
+            if (targetV > super.row) {
+                for (int i = super.row; i < targetV - 1; i++) {
+                    s = board[i][targetH.ordinal()];
+                    if (s.getPiece() != null) {
+                        return false;
+                    }
+                }
+            } //check if rook is moving down
+            else if (targetV < super.row) {
+                for (int i = targetV; i < super.row - 1; i++) {
+                    s = board[i][targetH.ordinal()];
+                    if (s.getPiece() != null) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        } //check if rook is moving horizontally
+        else if (targetH == super.column) {
+            Square s;
+            //check if moving to the right
+            if (targetH.ordinal() > super.column.ordinal()) {
+                for (Letter letter : Letter.values()) {
+                    if (letter.ordinal() > super.column.ordinal() && letter.ordinal() < targetH.ordinal()) {
+                        s = board[targetV][letter.ordinal()];
+                        if (s.getPiece() != null) {
+                            return false;
+                        }
+                    }
+                }
+            }
+            if (targetH.ordinal() < super.column.ordinal()) {
+                for (Letter letter : Letter.values()) {
+                    if (letter.ordinal() > targetH.ordinal() && letter.ordinal() < super.column.ordinal()) {
+                        s = board[targetV][letter.ordinal()];
+                        if (s.getPiece() != null) {
+                            return false;
+                        }
+                    }
+                }
+                return true;
+            }
+        }
+        return false;
+
+    }
+}
